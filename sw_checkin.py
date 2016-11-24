f9fade28a53c15c60a31b79562abde92aa3809f5
Matt Speakman <matthewspeakman@matthews-MBP.home>
2016-11-24 12:55:32 -0800

    Adding robobrowser

--- sw_checkin.py
+++ sw_checkin.py
@@ -9,30 +9,55 @@
 import requests
 import webbrowser
 import argparse
+from robobrowser import RoboBrowser
 
 def runCommandLine(url):
 	print("Going to %s" %(url))
 	print("I am checking you in via the command line.")
 
-def runBrowser(url):
-	print("I am checking you in via the web browser.")
-	webbrowser.open_new_tab(url)
+def runBrowser(url, confirmationNumber, firstName, lastName):
+	print("Checking in %s %s, with a confirmation number of %s." %(firstName, lastName, confirmationNumber))
+	success = False
+	browser = RoboBrowser(history=True)
+	browser.open(url)
+	confNum = browser.find(id="confirmationNumber")
+	confNum.value = confirmationNumber
+	fName = browser.find(id="firstName")
+	fName.value = firstName
+	lName = browser.find(id="lastName")
+	lName.value = lastName
+	#Click Submit Button
+	submitInfo = browser.find(id="")
+	#Check for Second Button
+	
+	#Scan for success
+	if success:
+		print("Thank you for checking in using sw_checkin.py. Enjoy your flight! :)")
+	else:
+		print("I'm terribly sorry... but I cannot guarantee that you were checked in.")
+		print("Please manually check so you get a good seat!")
 ########################################################################
 
 argParser = argparse.ArgumentParser(description="Check me into my flight so my seat doesn't suck!")
 
-argParser.add_argument("-w", "--web",action='store_true',help="This will open a browser so you can see the actions.")
-argParser.add_argument("-c","--cli",action='store_true',help="This operates on the command line. You will not see the browser.")
+#argParser.add_argument("-w", "--web",action='store_true',help="This will open a browser so you can see the actions.")
+argParser.add_argument("-c",help="This is your confirmation number.")
+argParser.add_argument("-f",help="This is your first name.")
+argParser.add_argument("-l",help="This is your last name.")
 
 args = argParser.parse_args()
-
+print(args)
 url = "https://www.southwest.com/flight/retrieveCheckinDoc.html?int=HOME-BOOKING-WIDGET-AIR-CHECKIN#js-booking-panel-check-in"
 
 
-if args.cli:
-	#Run Command Line
-	runCommandLine(url)
-elif args.web:
-	runBrowser(url)
-
-print("Thank you for checking in using sw_checkin.py. Enjoy your flight! :)")
+if args:
+	confirmationNumber = args.c
+	firstName = args.f
+	lastName = args.l
+	print(args.f)
+	runBrowser(url,confirmationNumber,firstName, lastName)
+else:
+	print("No arguments! POOR FORM OL' BOY")
+	exit(1)
+	
+exit(0)
