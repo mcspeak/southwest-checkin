d8dacb0e2583cb7647136ae830adcd424411e16f
mcspeak <mcspeak@umich.edu>
2017-05-11 14:51:17 -0500

    Filled out itineraryForm.

--- sw_checkin.py
+++ sw_checkin.py
@@ -20,14 +20,29 @@ def runBrowser(url, confirmationNumber, firstName, lastName):
 	success = False
 	browser = RoboBrowser(history=True)
 	browser.open(url)
-	confNum = browser.find(id="confirmationNumber")
-	confNum.value = confirmationNumber
-	fName = browser.find(id="firstName")
-	fName.value = firstName
-	lName = browser.find(id="lastName")
-	lName.value = lastName
+	
+	itineraryForm = browser.get_form(id="itineraryLookup")
+	print("Printing form before values: ")
+	print(itineraryForm)
+	
+	print("Value before: "+ str(itineraryForm['confirmationNumber'].value))
+	itineraryForm['confirmationNumber'].value = confirmationNumber
+	print("Value after: " + str(itineraryForm['confirmationNumber'].value))
+	
+	print("Value before: "+ str(itineraryForm['firstName'].value))
+	itineraryForm['firstName'].value = firstName
+	print("Value after: " + str(itineraryForm['firstName'].value))
+	
+	print("Value before: "+ str(itineraryForm['lastName'].value))
+	itineraryForm['lastName'].value = lastName
+	print("Value after: " + str(itineraryForm['lastName'].value))
+	
 	#Click Submit Button
-	submitInfo = browser.find(id="")
+	#submitInfo = browser.find(id="")
+	
+	print("Printing form before submit: ")
+	print(itineraryForm)
+	browser.submit_form(itineraryForm)
 	#Check for Second Button
 	
 	#Scan for success
@@ -46,11 +61,10 @@ argParser.add_argument("-f",help="This is your first name.")
 argParser.add_argument("-l",help="This is your last name.")
 
 args = argParser.parse_args()
-print(args)
+print("args are : " + str(args))
 url = "https://www.southwest.com/flight/retrieveCheckinDoc.html?int=HOME-BOOKING-WIDGET-AIR-CHECKIN#js-booking-panel-check-in"
 
-
-if args:
+if(args.c is not None and args.f is not None and args.l is not None):
 	confirmationNumber = args.c
 	firstName = args.f
 	lastName = args.l
